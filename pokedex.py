060bef4cd23041632e997962fcf0902dad76b4b6
Peter Hajas <peterhajas@gmail.com>
2014-12-29 17:06:10 -0600

    Extract forms from the pokedex data file. Still not displaying or storing this in user profiles, but it's a start

--- pokedex.py
+++ pokedex.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 class NationalDex:
     def __init__(self, pathToNationalDex):
@@ -7,11 +8,20 @@ class NationalDex:
         self.numberOfPokemon = len(self.dexdata.keys())
         self.pokemonNames = []
         self.pokemonSlugs = []
+        self.pokemonForms = defaultdict(list)
 
         for i in range (1, self.numberOfPokemon+1):
             dexKey = str(i).zfill(3)
             name = self.dexdata[dexKey]['name']['eng']
             slug = self.dexdata[dexKey]['slug']['eng']
+            forms = self.dexdata[dexKey]['icons']
+
             self.pokemonNames.append(name)
             self.pokemonSlugs.append(slug)
-
+            
+            for form in forms:
+                if form == '.':
+                    if 'has_female' in forms['.'].keys():
+                        self.pokemonForms[dexKey].append('female')
+                    continue
+                self.pokemonForms[dexKey].append(form)
