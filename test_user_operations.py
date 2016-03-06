dba522fad273923beeffbdf83ba602d12a6b08bc
Peter Hajas <peterhajas@gmail.com>
2016-03-06 15:01:26 -0800

    Add tests for user comparison

--- test_user_operations.py
+++ test_user_operations.py
@@ -20,21 +20,33 @@ class TestUserOperations(unittest.TestCase):
         self.userDB = UserDatabase()
         self.appDB = FakeDatabase()
         self.appDBSession = self.appDB.session
+
+        self.register_ash()
+        self.register_misty()
     def tearDown(self):
         self.userDB = None
         self.appDB = None
         self.appDBSession = None
+
+    # Convenience
     def register_ash(self):
         self.userDB.registerUser("AshKetchum", "pallettown123", "000000000000", self.appDB)
+    def register_misty(self):
+        self.userDB.registerUser("Misty", "cerulean456", "111111111111", self.appDB)
+    def ash_user(self):
+        return self.appDBSession.things[0]
+    def misty_user(self):
+        return self.appDBSession.things[1]
 
+    # Registration
     def test_registration(self):
-        self.register_ash()
-        user = self.appDBSession.things[0]
+        user = self.ash_user()
         self.assertEqual(user.username, "AshKetchum")
         self.assertTrue(self.appDB.session.didCommit)
+
+    # Catching / Releasing
     def test_catching_pokemon_works(self):
-        self.register_ash()
-        user = self.appDBSession.things[0]
+        user = self.ash_user()
         # Catch Pikachu
         self.userDB.catchPokemonForUser(user, 25, self.appDB)
         # Grab the state of the Pokemon @ 25
@@ -42,16 +54,14 @@ class TestUserOperations(unittest.TestCase):
         state = int(state)
         self.assertEqual(state, 1)
     def test_new_user_hasnt_caught_pokemon(self):
-        self.register_ash()
-        user = self.appDBSession.things[0]
+        user = self.ash_user()
         # Grab the state of the Pokemon @ 25
         # It should be 0
         state = self.userDB._stateOfPokemonForUser(user, 25, self.appDB)
         state = int(state)
         self.assertEqual(state, 0)
     def test_catching_then_releasing_pokemon_works(self):
-        self.register_ash()
-        user = self.appDBSession.things[0]
+        user = self.ash_user()
         # Catch Pikachu
         self.userDB.catchPokemonForUser(user, 25, self.appDB)
         # Release Pikachu :-(
@@ -61,6 +71,38 @@ class TestUserOperations(unittest.TestCase):
         state = int(state)
         self.assertEqual(state, 0)
 
+    # Comparison
+    def test_comparing_user_without_pokemon_and_user_without_pokemon_returns_neither_having_pokemon(self):
+        ash = self.ash_user()
+        misty = self.misty_user()
+        compared = self.userDB.comparedDexBetweenUsers(ash, misty, 1000)
+        # Sigh... 24 is Pikachu (because index 0 is Pokemon #1, Bulbasaur)
+        state_of_pikachu = int(compared[24])
+        self.assertEqual(state_of_pikachu, 0)
+    def test_comparing_user_with_pokemon_and_user_without_pokemon_returns_first_having_pokemon(self):
+        ash = self.ash_user()
+        misty = self.misty_user()
+        self.userDB.catchPokemonForUser(ash, 25, self.appDB)
+        compared = self.userDB.comparedDexBetweenUsers(ash, misty, 1000)
+        state_of_pikachu = int(compared[24])
+        self.assertEqual(state_of_pikachu, 1)
+    def test_comparing_user_without_pokemon_and_user_with_pokemon_returns_second_having_pokemon(self):
+        ash = self.ash_user()
+        misty = self.misty_user()
+        self.userDB.catchPokemonForUser(misty, 116, self.appDB)
+        compared = self.userDB.comparedDexBetweenUsers(ash, misty, 1000)
+        # 115 is Horsea (#116)
+        state_of_horsea = int(compared[115])
+        self.assertEqual(state_of_horsea, 2)
+    def test_comparing_user_with_pokemon_and_user_with_pokemon_returns_both_having_pokemon(self):
+        ash = self.ash_user()
+        misty = self.misty_user()
+        self.userDB.catchPokemonForUser(ash, 128, self.appDB)
+        self.userDB.catchPokemonForUser(misty, 128, self.appDB)
+        compared = self.userDB.comparedDexBetweenUsers(ash, misty, 1000)
+        # 127 is Tauros (#128)
+        state_of_tauros = int(compared[127])
+        self.assertEqual(state_of_tauros, 3)
 
 if __name__ == '__main__':
     unittest.main()
